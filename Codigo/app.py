6e18a5da955be18de8c643b22d4fe1880c06a029
kmi916 <manriqandres916k@gmail.com>
2024-07-26 16:08:04 -0500

    commit 3

---
+++ Codigo/app.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import pandas as pd
+from sodapy import Socrata
+
+st.title("Consulta de Datos de Contratación")
+
+# Configuración de la API Socrata
+client = Socrata("www.datos.gov", None)
+
+# Solicitud de datos
+@st.cache
+def load_data():
+    results = client.get("jbjy-vk9h", limit=1000)  # Reemplazar 'xxxx' con el endpoint específico
+    return pd.DataFrame.from_records(results)
+
+df = load_data()
+
+# Mostrar datos
+st.write(df)
